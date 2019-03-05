39667d26923ac47d3a2393b13ed863a78ab3938a
kosukekimura <kimkim5364@gmail.com>
2019-03-05 10:15:32 +0000

    complete all specifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get 'blogs/index', to:'blogs#index'
+  resources :blogs do
+    collection do
+      post 'confirm'
+    end
+  end
+
 end
