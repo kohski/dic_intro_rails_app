0fce26efd776d862f1bc1e1069fbe74d292e9a1c
kosukekimura <kimkim5364@gmail.com>
2019-03-05 11:17:24 +0000

    add back button on confirm page and change timing validation ignition

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,7 +7,11 @@ class BlogsController < ApplicationController
   end
 
   def new
-    @blog = Blog.new
+    if params[:back]
+      @blog = Blog.new(blog_params)  
+    else
+      @blog = Blog.new      
+    end
   end
 
   def create
@@ -24,6 +28,7 @@ class BlogsController < ApplicationController
   
   def confirm
     @blog = Blog.new(blog_params)
+    render 'new' if @blog.invalid?
   end
   
   
